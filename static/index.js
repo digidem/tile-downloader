20b2946db92ba22a6d930c00687cd060230ff963
karissa <krmckelv@gmail.com>
2018-04-19 00:44:54 +0200

    preview download before doing it

--- static/index.js
+++ static/index.js
@@ -12,7 +12,15 @@ var map = new mapboxgl.Map({
   style: 'mapbox://styles/mapbox/streets-v9'
 })
 
+var $overlay = document.getElementById('overlay')
+$overlay.addEventListener('click', closePreview)
+document.body.addEventListener('keydown', function (event) {
+  if (event.key === 'Escape') closePreview(event)
+})
 var $controls = document.getElementById('controls')
+$controls.addEventListener('click', function (event) {
+  event.stopPropagation()
+})
 var controls = createControls()
 $controls.appendChild(controls)
 
@@ -23,7 +31,7 @@ $buttons.appendChild(buttons)
 map.on('moveend', function () {
   var bbox = getMapBbox()
   var minZoom = Math.round(map.getZoom())
-  yo.update($controls, createControls(bbox, minZoom))
+  yo.update(controls, createControls(bbox, minZoom))
 })
 
 function getMapBbox (bbox) {
@@ -40,49 +48,61 @@ function getFormData () {
   return data
 }
 
-function onclick (event) {
+function downloadClick (event) {
   var data = getFormData()
   download(data)
+  console.log('got', data)
   event.stopPropagation()
   event.preventDefault()
   return false
 }
 
-function flyToClick () {
-  $controls.setAttribute('style', 'display: block;')
-}
-
-function updateView (event) {
-  var data = getFormData()
-  map.fitBounds([[data.minLng, data.minLat], [data.maxLng, data.maxLat]])
-  map.setZoom(data.minZoom)
+function previewDownload (event) {
+  $overlay.style = 'display: block;'
   event.stopPropagation()
   event.preventDefault()
-  $controls.setAttribute('style', 'display: none;')
+  return false
+}
+
+function closePreview (event) {
+  $overlay.style = 'display: none;'
+  event.stopPropagation()
+  event.preventDefault()
   return false
 }
 
 function createControls (bbox, minZoom) {
-  if (!bbox) bbox = [0, 0, 0, 0]
-  if (!minZoom) minZoom = map.getZoom()
+  if (!bbox) bbox = getMapBbox()
+  if (!minZoom) map.getZoom()
   var maxZoomEl = document.querySelector('input[name="maxZoom"]')
   if (maxZoomEl) maxZoom = maxZoomEl.value
-  return yo`<form onsubmit="${onclick}">
-    <h3>Bounding Box</h3>
-    <input type="text" name="minLng" value=${bbox[0]}/>
-    <input type="text" name="minLat" value=${bbox[1]}/>
-    <input type="text" name="maxLng" value=${bbox[2]} />
-    <input type="text" name="maxLat" value=${bbox[3]} />
+  return yo`<form onsubmit=${downloadClick}>
+    <p>Bounding Box</p>
+    Min Lng: <input type="text" name="minLng" value=${bbox[0]}/>
     <br>
+    Min Lat: <input type="text" name="minLat" value=${bbox[1]}/>
+    <br>
+    Max Lng: <input type="text" name="maxLng" value=${bbox[2]} />
+    <br>
+    Max Lat: <input type="text" name="maxLat" value=${bbox[3]} />
+    <br>
+    <p>Zoom Range</p>
     <input type="text" name="minZoom" value=${minZoom} />
     <input type="text" name="maxZoom" value=${maxZoom} />
-    <button onclick=${updateView}>OK</button>
+    <br>
+    <p>Estimated Size: TODO</p>
+    <button onclick=${closePreview}>Just Kidding</button>
+    <button type="submit">Start Downloading</button>
   </from>`
+}
+
+function flyToCoordinates (data) {
+  map.fitBounds([[data.minLng, data.minLat], [data.maxLng, data.maxLat]])
+  map.setZoom(data.minZoom)
 }
 
 function createButtons () {
   return yo`<div>
-    <button onclick=${flyToClick}>Fly To Coordinates...</button>
-    <button type="submit">Download Tiles</button>
+    <button onclick=${previewDownload}>Ready to Download Tiles? Click here to preview...</button>
   </div>`
 }
